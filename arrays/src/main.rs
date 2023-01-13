0af54d956583939e40d17c5fac9d256ca63ecf27
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-13 06:50:40 -0800

    Added first two projects

---
+++ arrays/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main() {
+
+    // array (immutable)
+    let arr = [0, 1, 2, 3, 4, 5];
+
+    loop {
+
+        println!("Please enter an array index.");
+        let mut index = String::new();
+        
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line");
+
+        let index: usize = index
+            .trim()
+            .parse()
+            .expect("Invalid index");
+
+        if index >= arr.len() {
+            println!("Invalid index");
+            continue;
+        }
+
+        let element = arr[index];
+
+        println!("The value of the elemnt at index {index} is {element}");
+
+    }
+}
