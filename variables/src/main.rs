b8492133ede646ed08903963cdbc322a7080c0b3
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-06 20:13:38 -0800

    added variables project

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+   
+    // Variables are immutable by default
+    let mut x = 5;
+    println!("The value of x is: {x}");
+
+    // Mutable variables can be changed
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // Constants
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("{}",THREE_HOURS_IN_SECONDS);
+}
