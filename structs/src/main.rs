1931f99b120ae1a0388f801ea954689e79123040
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-08 17:00:29 -0800

    fixed whitespace

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,4 +1,4 @@
- struct User {
+struct User {
     active: bool,
     name: String,
     age: u16,    
@@ -26,9 +26,9 @@ fn main() {
 }
 
 fn print_user(user: &User) {
-     println!("Name: {}", user.name);
-     println!("Age: {}", user.age);
-     println!("Status: {}\n", user.active);
+    println!("Name: {}", user.name);
+    println!("Age: {}", user.age);
+    println!("Status: {}\n", user.active);
 }
 
 fn build_user(name: String, age: u16) -> User {
