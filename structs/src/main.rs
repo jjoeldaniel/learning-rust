b5e1557a803c761532416c289ac58a5552318957
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-02-08 16:47:43 -0800

    added struct update example

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -12,8 +12,15 @@ fn main() {
         age: 22,
     };
 
-    let name: String = user1.name;
+    let user2 = User {
+        name: String::from("Not Joel"),
+        ..user1
+    };
 
-    println!("{name}");
+    let name1: String = user1.name;
+    let name2: String = user2.name;
+
+    println!("Name 1: {name1}");
+    println!("Name 2: {name2}");
 
 }
